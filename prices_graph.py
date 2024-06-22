e724e6008a68d7cf38c9a1adb4341749e4548773
riley <riso7312@uni.sydney.edu.au>
2024-06-22 23:45:11 +1000

    graph

--- prices_graph.py
+++ prices_graph.py
@@ -1,17 +1,3 @@
-# import matplotlib
-# import matplotlib.pyplot as plt
-# import matplotlib.ticker as mticker
-# import matplotlib.dates as mdates
-# import numpy as np
-
-# def graph():
-#     data = np.loadtxt('prices.txt', dtype = float)
-#     fig = plt.figure(figsize = (10,7))
-#     ax1 = plt.subplot2grid((40,40), (0,0), rowspan = 40, colspan = 40)
-    
-#     ax1.plot()
-# graph()
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -28,17 +14,11 @@ def plotPrices(prices, output_file):
     plt.xlabel('Days')
     plt.ylabel('Price')
     plt.title('Price History of Instruments')
-    # plt.legend(loc='upper left')
     plt.savefig(output_file)  # Save the plot as an image file
     plt.close()  # Close the plot to free up memory
-
-# File containing the prices
 pricesFile = "prices.txt"
 
-# Load and transpose the data so that rows are instruments and columns are days
 prcAll = loadPrices(pricesFile).T
-
-# Specify the output file
 output_file = "price_history.png"
 
 # Plot the data and save it as an image
